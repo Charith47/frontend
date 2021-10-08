01841a472c881b368953572e942fe8e877429ebd
Charith Dissanayake <charithdissanayake47@gmail.com>
2021-10-08 19:36:51 +0530

    scanner layout edit

---
+++ src/components/TicketCard.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-container>
+		<v-card flat class="text-xs ma-3">
+			<v-card-text>
+				<div class="id">{{ id }}</div>
+				<div class="route">{{ start }} to {{ end }}</div>
+				<div class="price">{{ price }} LKR</div>
+			</v-card-text>
+			<v-card-actions>
+				<v-btn color="light-grey" @click="isSelected = !isSelected" :style="isSelected ? 'border: 1px solid red;' : null">
+					Select Route
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+export default {
+	props: ['id', 'start', 'end', 'price'],
+	data() {
+		return {
+			isSelected: false,
+		};
+	},
+	name: 'TicketCard',
+};
+</script>
--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,9 +1,43 @@
 <template>
- <h1>Tickets</h1>
+	<div class="container">
+		<h1>Tickets</h1>
+		<p>
+			Select one of your tickets to start your ride
+		</p>
+		<v-container>
+			<v-row class="d-flex flex-column">
+				<TicketCard
+					v-for="ticket in tickets"
+					:key="ticket.id"
+					:id="ticket.id"
+					:start="ticket.start"
+					:end="ticket.end"
+					:price="ticket.price"
+				></TicketCard>
+			</v-row>
+		</v-container>
+	</div>
 </template>
 
 <script>
-  export default {
-    name: 'Tickets',
-  }
+import TicketCard from '../components/TicketCard.vue';
+
+export default {
+	components: {
+		TicketCard,
+	},
+
+	data() {
+		return {
+			selected:[],
+			tickets: [
+				{ id: 1, start: 'Ratmalana', end: 'Colombo', price: 100 },
+				{ id: 2, start: 'Galle', end: 'Colombo', price: 200 },
+				{ id: 3, start: 'Nugegoda', end: 'Dehiwala', price: 300 },
+				{ id: 4, start: 'Matara', end: 'Galle', price: 400 },
+			],
+		};
+	},
+	name: 'Tickets',
+};
 </script>
