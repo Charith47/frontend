3c052eaa64e89c485d2a7fb84fb0af8b21746004
Charith Dissanayake <charithdissanayake47@gmail.com>
2021-10-09 14:37:05 +0530

    account page fetch

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,14 +2,13 @@
 	<v-container>
 		<v-card class="d-flex justify-center" flat color="transparent">
 			<v-avatar color="indigo" size="128">
-				<span>MK</span>
+				<span>{{ this.user.displayName.split('')[0] }}</span>
 			</v-avatar>
 		</v-card>
 		<v-card flat outlined class="px-2 py-2">
+			<span>{{ this.user.displayName }}<br /></span>
+			<span>{{ this.user.email }}</span>
 
-			<span>{{this.getUser.displayName}}<br></span>
-			<span>{{this.getUser.email}}</span>
-			
 			<v-btn @click="logout" block color="primary">
 				Logout
 			</v-btn>
@@ -22,6 +21,11 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 export default {
+	mounted() {
+		console.log('Account page mounted');
+		this.user = firebase.auth().currentUser;
+		console.log(this.user);
+	},
 	methods: {
 		async logout() {
 			try {
@@ -35,15 +39,9 @@ export default {
 	},
 	data() {
 		return {
+			user: '',
 			loggedIn: false,
 		};
-	},
-	computed: {
-		getUser() {
-			const user = firebase.auth().currentUser;
-			console.log(user);
-			return user;
-		},
 	},
 	name: 'Account',
 };
