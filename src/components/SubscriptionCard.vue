e58c5ce1dfb3416916d417b0192c29d9ccb66f3d
lithmaP <lithmaperera@gmail.com>
2021-10-09 14:31:46 +0530

    adding Tickets_UI changes

---
+++ src/components/SubscriptionCard.vue
@@ -0,0 +1,38 @@
+<template>
+
+ <v-container>
+     <v-card flat class="text-xs ma-3">
+        <v-card-text>
+        <div class="id">Subscription ID : {{id}}</div> 
+        <div class="route">Route : {{start}} to {{end}}</div>
+        <div class="issuedDate">Issued Date : {{issuedDate}}</div>
+        <div class="expiryDate">Expiry Date : {{expiryDate}}</div>
+            <div class="price">LKR {{price}}</div>
+    </v-card-text>
+    <v-card-actions>
+        <v-btn flat color="light-grey">
+            Select subcription
+        </v-btn>
+    </v-card-actions>
+    </v-card>
+ </v-container>
+    
+</template>
+
+<script>
+
+export default ({
+    props:['id','start','end','issuedDate','expiryDate','price'],
+    name:'SubscriptionCard'
+   
+})
+</script>
+
+<style scoped>
+.subscriptionCard {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
---
+++ src/components/TicketCard.vue
@@ -0,0 +1,36 @@
+<template>
+
+ <v-container>
+     <v-card flat class="text-xs ma-3">
+        <v-card-text>
+        <div class="id">Ticket ID : {{id}}</div>
+        <div class="route">Route : {{start}} to {{end}}</div>
+            <div class="price">Tciket price : LKR {{price}}</div>
+    </v-card-text>
+    <v-card-actions>
+        <v-btn flat color="light-grey">
+            Select Route
+        </v-btn>
+    </v-card-actions>
+    </v-card>
+ </v-container>
+    
+</template>
+
+<script>
+
+export default ({
+    props:['id','start','end','price'],
+    name:'TicketCard'
+   
+})
+</script>
+
+<style scoped>
+.ticketCard {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
